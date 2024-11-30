809771108c352a54c5afe8ca901a4325b5dddce5
MaikVuolo <maikvb88@gmail.com>
2024-11-30 07:33:45 -0300

    style: :art: Organização de pastas e rotas

--- src/rotas/Favoritos.js
+++ src/rotas/Favoritos.js
@@ -1,7 +1,5 @@
-import Header from './componentes/Header';
 import styled from 'styled-components';
-import Pesquisa from './componentes/Pesquisa';
-import UltimosLancamentos from './componentes/UltimosLancamentos';
+import Pesquisa from '../componentes/Pesquisa';
 
 const AppContainer = styled.div`
   width: 100vw;
@@ -10,14 +8,12 @@ const AppContainer = styled.div`
 
 `
 
-function App() {
+function Favoritos() {
   return (
     <AppContainer>
-      <Header/>
       <Pesquisa/>
-      <UltimosLancamentos/>
     </AppContainer>
   );
 }
 
-export default App;
+export default Favoritos;
